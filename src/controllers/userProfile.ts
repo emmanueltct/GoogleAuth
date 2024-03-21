a239a380a4a23c0af1bb5ad5b92725181a06cf3d
uwimana janet <janetbarbie@uwimanas-MacBook-Pro.local>
2024-03-21 22:16:18 +0200

    adding email notification

--- src/controllers/userProfile.ts
+++ src/controllers/userProfile.ts
@@ -1,13 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import mailNotification from '../notification/notification';
 
 
 const userProfile=async(req:Request,res:Response)=>{
-
+    console.log('hello');
+ 
 }
 
 
 const updateProfile=async(req:Request,res:Response)=>{
-
+    const userData={
+        name:'Prince Rwigimba',
+        profile:'profile',
+    }
+     const email= 'emmanuelmunezero@gmail.com';
+mailNotification(userData,email)
+return res.send();
 }
 
 
